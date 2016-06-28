bbec9cc0653df4c2f8b4d9e23d794a2cfd19cc0a
William W Wold <william01110111@gmail.com>
2016-06-28 14:51:55 -0700

    initial commit

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -1,20 +1,58 @@
-//
-//  ViewController.swift
-//  TipCalc
-//
-//  Created by William Wold on 6/20/16.
-//  Copyright © 2016 Widap. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var costInput: UITextField!
+	@IBOutlet weak var tipPercentInput: UISegmentedControl!
+	@IBOutlet weak var tipCostOutput: UITextField!
+	@IBOutlet weak var totalCostOutput: UITextField!
+	
+	let tips: [Double] = [15, 18, 20]
+	
+	func updateVals() {
+		
+		if let text=costInput.text, cost=Double(text) {
+			
+			let selectedTip = tips[tipPercentInput.selectedSegmentIndex]
+			let tipCost = (selectedTip/100.0)*cost
+			let totalCost=tipCost+cost
+			
+			tipCostOutput.text="Tip: $\(tipCost)"
+			totalCostOutput.text="Total: $\(totalCost)"
+		}
+		else {
+			
+			tipCostOutput.text="Tip"
+			totalCostOutput.text="Total"
+		}
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		
+		tipPercentInput.removeAllSegments()
+		
+		for i in 0..<tips.count {
+			
+			tipPercentInput.insertSegmentWithTitle("\(tips[i])%", atIndex: i, animated: true)
+		}
+		
+		tipPercentInput.selectedSegmentIndex = 0
+		
+		updateVals()
     }
-
+	
+	@IBAction func textTyped(sender: AnyObject) {
+		
+		updateVals()
+	}
+	
+	@IBAction func tipPercentSelected(sender: AnyObject) {
+		
+		updateVals()
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
